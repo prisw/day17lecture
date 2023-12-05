ed977edb0b0d49ccbe16d16a5ee072e47752c588
prisw <popcil@yahoo.com>
2023-12-05 10:42:08 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/day17lecture/Day17lectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day17lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day17lectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day17lectureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/sg/edu/nus/iss/day17lecture/controller/HomeController.java
@@ -0,0 +1,12 @@
+package sg.edu.nus.iss.day17lecture.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/home")
+public class HomeController {
+
+    
+    
+}
